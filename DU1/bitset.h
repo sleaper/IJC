85a13cbfce6cbbced90025d9114189b843c9ef0e
Petr Spac <petrik.spac@gmail.com>
2024-02-23 15:04:04 +0100

    bitset_fill

--- DU1/bitset.h
+++ DU1/bitset.h
@@ -28,20 +28,20 @@ typedef unsigned long bitset_index_t;
 // 64 = size[1 + 1] = [size, ulong]
 // 100 = size[1 + 2] = [size, ulong, ulong]
 #define bitset_create(jmeno_pole, velikost)                                    \
-	unsigned long(jmeno_pole)[BITSET_NUM_ULONGS(velikost) +                    \
-							  (BIT_POS_IN_ULONG(velikost) ? 2 : 1)] = {        \
-		velikost};                                                             \
-	static_assert((velikost) > 0, "bitset_create: Pole musi byt vetsi nez 0")
+    unsigned long(jmeno_pole)[BITSET_NUM_ULONGS(velikost) +                    \
+                              (BIT_POS_IN_ULONG(velikost) ? 2 : 1)] = {        \
+        velikost};                                                             \
+    static_assert((velikost) > 0, "bitset_create: Pole musi byt vetsi nez 0")
 
 #define bitset_alloc(jmeno_pole, velikost)                                     \
-	assert((unsigned long)velikost < ULONG_MAX);                               \
-	unsigned long *jmeno_pole = calloc(                                        \
-		(BITSET_NUM_ULONGS(velikost) + (BIT_POS_IN_ULONG(velikost) ? 2 : 1)),  \
-		(sizeof(unsigned long)));                                              \
-	if (jmeno_pole == NULL)                                                    \
-		error_exit("bitset_alloc: Chyba lokace pameti");                       \
+    assert((unsigned long)velikost < ULONG_MAX);                               \
+    unsigned long *jmeno_pole = calloc(                                        \
+        (BITSET_NUM_ULONGS(velikost) + (BIT_POS_IN_ULONG(velikost) ? 2 : 1)),  \
+        (sizeof(unsigned long)));                                              \
+    if (jmeno_pole == NULL)                                                    \
+        error_exit("bitset_alloc: Chyba lokace pameti");                       \
                                                                                \
-	jmeno_pole[0] = velikost;
+    jmeno_pole[0] = velikost;
 
 #define bitset_free(jmeno_pole) free(jmeno_pole);
 
@@ -53,36 +53,36 @@ inline unsigned long bitset_size(bitset_t jmeno_pole) { return jmeno_pole[0]; }
 
 // Checks for index bounds and modifies the specified bit accordingly.
 inline void bitset_setbit(bitset_t jmeno_pole, unsigned long index, int vyraz) {
-	if (index >= bitset_size(jmeno_pole)) {
-		error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu",
-				   (unsigned long)index,
-				   (unsigned long)bitset_size(jmeno_pole) - 1);
-	}
+    if (index >= bitset_size(jmeno_pole)) {
+        error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu",
+                   (unsigned long)index,
+                   (unsigned long)bitset_size(jmeno_pole) - 1);
+    }
 
-	if (vyraz) {
-		jmeno_pole[BITSET_NUM_ULONGS(index) + 1] |=
-			(1UL << BIT_POS_IN_ULONG(index));
-	} else {
-		jmeno_pole[BITSET_NUM_ULONGS(index) + 1] &=
-			~(1UL << BIT_POS_IN_ULONG(index));
-	}
+    if (vyraz) {
+        jmeno_pole[BITSET_NUM_ULONGS(index) + 1] |=
+            (1UL << BIT_POS_IN_ULONG(index));
+    } else {
+        jmeno_pole[BITSET_NUM_ULONGS(index) + 1] &=
+            ~(1UL << BIT_POS_IN_ULONG(index));
+    }
 
-	return;
+    return;
 }
 
 inline unsigned long bitset_getbit(bitset_t jmeno_pole, unsigned long index) {
-	if (index >= bitset_size(jmeno_pole)) {
-		error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu",
-				   (unsigned long)index,
-				   (unsigned long)bitset_size(jmeno_pole) - 1);
-	} else {
-		return jmeno_pole[BITSET_NUM_ULONGS(index) + 1] &
-					   (1UL << BIT_POS_IN_ULONG(index))
-				   ? 1UL
-				   : 0UL;
-	}
+    if (index >= bitset_size(jmeno_pole)) {
+        error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu",
+                   (unsigned long)index,
+                   (unsigned long)bitset_size(jmeno_pole) - 1);
+    } else {
+        return jmeno_pole[BITSET_NUM_ULONGS(index) + 1] &
+                       (1UL << BIT_POS_IN_ULONG(index))
+                   ? 1UL
+                   : 0UL;
+    }
 
-	return 3;
+    return 3;
 }
 
 #else
@@ -90,25 +90,44 @@ inline unsigned long bitset_getbit(bitset_t jmeno_pole, unsigned long index) {
 #define bitset_size(jmeno_pole) jmeno_pole[0]
 
 #define bitset_setbit(jmeno_pole, index, vyraz)                                \
-	if (((unsigned long)(index) >= (jmeno_pole[0])))                           \
-		error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu",              \
-				   (unsigned long)index,                                       \
-				   (unsigned long)bitset_size(jmeno_pole) - 1);                \
-	vyraz ? ((jmeno_pole[BITSET_NUM_ULONGS(index) + 1]) |=                     \
-			 (1UL << BIT_POS_IN_ULONG(index)))                                 \
-		  : ((jmeno_pole[BITSET_NUM_ULONGS(index) + 1]) &=                     \
-			 ~(1UL << BIT_POS_IN_ULONG(index)))
+    if (((unsigned long)(index) >= (jmeno_pole[0])))                           \
+        error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu",              \
+                   (unsigned long)index,                                       \
+                   (unsigned long)bitset_size(jmeno_pole) - 1);                \
+    vyraz ? ((jmeno_pole[BITSET_NUM_ULONGS(index) + 1]) |=                     \
+             (1UL << BIT_POS_IN_ULONG(index)))                                 \
+          : ((jmeno_pole[BITSET_NUM_ULONGS(index) + 1]) &=                     \
+             ~(1UL << BIT_POS_IN_ULONG(index)))
 
 #define bitset_getbit(jmeno_pole, index)                                       \
-	(((unsigned long)(index) >= bitset_size(jmeno_pole))                       \
-		 ? (error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu",          \
-					   (unsigned long)index,                                   \
-					   (unsigned long)bitset_size(jmeno_pole) - 1),            \
-			3)                                                                 \
-	 : (jmeno_pole[BITSET_NUM_ULONGS(index) + 1] &                             \
-		(1UL << BIT_POS_IN_ULONG(index)))                                      \
-		 ? 1UL                                                                 \
-		 : 0UL)
+    (((unsigned long)(index) >= bitset_size(jmeno_pole))                       \
+         ? (error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu",          \
+                       (unsigned long)index,                                   \
+                       (unsigned long)bitset_size(jmeno_pole) - 1),            \
+            3)                                                                 \
+     : (jmeno_pole[BITSET_NUM_ULONGS(index) + 1] &                             \
+        (1UL << BIT_POS_IN_ULONG(index)))                                      \
+         ? 1UL                                                                 \
+         : 0UL)
+
+#define bitset_fill(jmeno_pole, bool_výraz)                                    \
+    if (!(bool_výraz)) {                                                       \
+        for (unsigned long i = 1;                                              \
+             (i) < (BITSET_NUM_ULONGS(bitset_size(jmeno_pole)) +               \
+                    (BIT_POS_IN_ULONG(bitset_size(jmeno_pole)) ? 2 : 1));      \
+             i++) {                                                            \
+            (jmeno_pole[i]) = 0;                                               \
+        }                                                                      \
+    } else {                                                                   \
+        for (unsigned long i = 1;                                              \
+             (i) < (BITSET_NUM_ULONGS(bitset_size(jmeno_pole)) +               \
+                    (BIT_POS_IN_ULONG(bitset_size(jmeno_pole)) ? 2 : 1));      \
+             i++) {                                                            \
+            for (unsigned long bit = 0; (bit) < (ULONG_SIZE); bit++) {         \
+                (jmeno_pole[i]) |= (1UL << bit);                               \
+            }                                                                  \
+        }                                                                      \
+    }
 
 #endif
 
--- DU1/primes.c
+++ DU1/primes.c
@@ -1,34 +1,56 @@
 #include "bitset.h"
 #include "eratosthenes.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 // TODO: CHANGE FOR DU
-#define N 230000000
+// #define N 666000000
+#define N 100
 #define LAST_TEN 10
+
+void print_bits(unsigned long num) {
+    for (unsigned long bit = 0; bit < (sizeof(unsigned long) * 8); bit++) {
+        printf("%lu ", num & 0x01);
+        num = num >> 1;
+    }
+    printf("\n");
+}
 
 int main(void) {
 
-	clock_t start = clock();
+    bitset_alloc(p, N);
+    printf("unsigned long: %lu\n", sizeof(unsigned long) * CHAR_BIT);
 
-	unsigned long nums[LAST_TEN] = {0};
-	bitset_create(p, N);
+    printf("p[0] = %lu\n", p[0]);
 
-	Eratosthenes(p);
+    print_bits(p[1]);
+    bitset_fill(p, 1);
+    print_bits(p[1]);
+    print_bits(p[2]);
+    bitset_free(p);
 
-	// Load last 10 prime numbers
-	int count = 0;
-	for (unsigned long i = bitset_size(p) - 1; count < LAST_TEN; i--) {
-		if (bitset_getbit(p, i) == 0) {
-			nums[count++] = i;
-		}
-	}
+    // clock_t start = clock();
+    //
+    // unsigned long nums[LAST_TEN] = {0};
+    // bitset_create(p, N);
+    //
+    // Eratosthenes(p);
+    //
+    // // Load last 10 prime numbers
+    // int count = 0;
+    // for (unsigned long i = bitset_size(p) - 1; count < LAST_TEN; i--) {
+    // 	if (bitset_getbit(p, i) == 0) {
+    // 		nums[count++] = i;
+    // 	}
+    // }
+    //
+    // for (int i = LAST_TEN - 1; i >= 0; i--) {
+    // 	printf("%lu\n", nums[i]);
+    // }
+    //
+    // fprintf(stderr, "Time=%.3g\n", (double)(clock() - start) /
+    // CLOCKS_PER_SEC);
 
-	for (int i = LAST_TEN - 1; i >= 0; i--) {
-		printf("%lu\n", nums[i]);
-	}
-
-	fprintf(stderr, "Time=%.3g\n", (double)(clock() - start) / CLOCKS_PER_SEC);
-
-	return 0;
+    return 0;
 }
